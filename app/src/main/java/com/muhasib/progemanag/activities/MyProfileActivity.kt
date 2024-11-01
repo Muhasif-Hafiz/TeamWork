a146bd40a8e3479a068fd5fe9ccac37e1b47752a
Muhasif Hafiz <muhasibhafeez91@gmail.com>
2024-11-01 18:11:23 +0530

    Adding Photo to th profile Section

--- app/src/main/java/com/muhasib/progemanag/activities/MyProfileActivity.kt
+++ app/src/main/java/com/muhasib/progemanag/activities/MyProfileActivity.kt
@@ -1,16 +1,34 @@
 package com.muhasib.progemanag.activities
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
+
 import com.muhasib.progemanag.R
 import com.muhasib.progemanag.models.User
 import com.projemanag.firebase.FirestoreClass
 import de.hdodenhof.circleimageview.CircleImageView
+import java.io.IOException
 
 class MyProfileActivity : BaseActivity() {
+    lateinit var iv_profile_user_image:  ImageView
+
+    companion object{
+        private const val READ_STORAGE_PERMISSION_CODE =1
+        private const val  PICK_IMAGE_REQUEST_CODE=2
+    }
+    private var mSelectedImage : Uri? =null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +38,60 @@ class MyProfileActivity : BaseActivity() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar_my_profile_activity)
         setUpActionBar(toolbar)
 
+        iv_profile_user_image=findViewById(R.id.iv_profile_user_image)
+        iv_profile_user_image.setOnClickListener {
+            if(ContextCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_GRANTED) {
+
+                showImageChooser()
+
+
+            }else{
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),
+                    READ_STORAGE_PERMISSION_CODE)
+            }
+        }
         FirestoreClass().loadUserData(this)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode== READ_STORAGE_PERMISSION_CODE){
+            if(grantResults.isNotEmpty() && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                showImageChooser()
+            }else{
+                Toast.makeText(this, " OOPS! you just Denied the permission, You can Allow it from the Storage", Toast.LENGTH_LONG).show()
+
+            }
+        }
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode== Activity.RESULT_OK && requestCode == PICK_IMAGE_REQUEST_CODE && data!!.data!=null){
+
+            mSelectedImage=data.data
+
+            try {
+                Glide.with(this)
+                    .load(mSelectedImage)
+                    .centerCrop()
+                    .placeholder(R.drawable.ic_user_place_holder)
+                    .into(iv_profile_user_image)
+            }catch (e:IOException){
+                e.printStackTrace()
+            }
+        }
+    }
+
+
+    private fun showImageChooser(){
+        val galleryIntent=Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(galleryIntent, PICK_IMAGE_REQUEST_CODE)
     }
 
     private fun setUpActionBar(toolbar: Toolbar) {
@@ -34,7 +105,7 @@ class MyProfileActivity : BaseActivity() {
     }
 
     fun setUserDataInUI(user: User) {
-        val ivUserImage = findViewById<CircleImageView>(R.id.iv_user_image)
+        val ivUserImage = findViewById<CircleImageView>(R.id.iv_profile_user_image)
         val etName = findViewById<EditText>(R.id.et_name)
         val etEmail = findViewById<EditText>(R.id.et_email)
         val etMobile = findViewById<EditText>(R.id.et_mobile)
